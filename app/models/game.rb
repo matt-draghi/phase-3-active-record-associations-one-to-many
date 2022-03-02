da8266c0001d29c406c7c14cb9e9298556a32861
Matt Draghi <mtd5264@gmail.com>
2022-03-02 17:01:04 -0500

    commit tree ffc0a1b11961ab512b475f7bdb83f10bc9cbb628

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
+    #has_many :reviews is equivalent to this...
+        #   def reviews
+        #     Review.where(game_id: self.id)
+        #   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
-  
+    belongs_to :game #this gets rid of the need to create the method below
+        # def game
+        #     # self is the review instance
+        #     Game.find(self.game_id)
+        #   end
 end
